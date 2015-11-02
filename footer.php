be5afcb035b830b52947627d5bc3d7b88f24f883
Leonid <leonitebelov@gmail.com>
2015-11-02 10:29:07 +0300

    footer menu

--- footer.php
+++ footer.php
@@ -1,13 +1,43 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
 				<div id="inner-footer" class="wrap cf" style="margin:0 auto;">
-
+				
 					<nav role="navigation">
-						<?php wp_nav_menu(array(
+					
+						<div id="footer-links1">
+						
+						<?php
+							
+							if(is_active_sidebar('footer-links-1')){
+								
+								dynamic_sidebar('footer-links-1');
+							
+							}
+						
+						?>
+						
+						</div>
+
+
+						<div id="footer-links2">
+						
+						<?php
+
+							if(is_active_sidebar('footer-links-2')){
+
+								dynamic_sidebar('footer-links-2');
+						
+							}
+						
+						?>
+
+						</div>
+						
+						<?php /*wp_nav_menu(array(
     					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
     					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
-    					'menu' => __( 'Footer Links', 'leonitetheme' ),   // nav name
-    					'menu_class' => '',            // adding custom nav class
+    					'menu' => __( 'Footer Links', 'leonite' ),   // nav name
+    					'menu_class' => 'double',            // adding custom nav class
     					'theme_location' => 'footer-links',             // where it's located in the theme
     					'before' => '',                                 // before the menu
     					'after' => '',                                  // after the menu
@@ -15,18 +45,25 @@
     					'link_after' => '',                             // after each link
     					'depth' => 0,                                   // limit the depth of the nav
     					'fallback_cb' => 'leonite_footer_links_fallback'  // fallback function
-						)); ?>
+						)); */?>
+						
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					
 
+				</div>
+				
+				<div class="copyright">
+					
+						<p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Legal notices</p>
+						
 				</div>
 
 			</footer>
 
 		</div>
 
-		<?php // all js scripts are loaded in library/leonite.php ?>
+		<?php // all js scripts are loaded in library/core.php ?>
 		<?php wp_footer(); ?>
 
 	</body>
--- functions.php
+++ functions.php
@@ -185,11 +185,33 @@ add_action( 'customize_register', 'leonite_theme_customizer' );
 function leonite_register_sidebars() {
 	register_sidebar(array(
 		'id' => 'sidebar1',
-		'name' => __( 'Sidebar 1', 'leonitetheme' ),
-		'description' => __( 'The first (primary) sidebar.', 'leonitetheme' ),
+		'name' => __( 'Sidebar 1', 'leonite' ),
+		'description' => __( 'The first (primary) sidebar.', 'leonite' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+	
+	//footer sidebar menu links #1
+	register_sidebar(array(
+		'id' => 'footer-links-1',
+		'name' => __( 'Footer Links Sidebar', 'leonite' ),
+		'description' => __( 'Footer Links Sidebar', 'leonite' ),
+		'before_widget' => '<div id="%1$s" class="footer-links">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="footer-header">',
+		'after_title' => '</h4>',
+	));
+	
+	//footer sidebar menu links #2
+	register_sidebar(array(
+		'id' => 'footer-links-2',
+		'name' => __( 'Footer Links Sidebar 2', 'leonite' ),
+		'description' => __( 'Footer Links Sidebar 2', 'leonite' ),
+		'before_widget' => '<div id="%1$s" class="footer-links">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="footer-header">',
 		'after_title' => '</h4>',
 	));
 
