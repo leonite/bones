60b4dae3322b4a6e5f34438387f398a30d45094f
Leonite <leonitebelov@gmail.com>
2015-12-04 17:02:55 +0300

    archive template

--- archive.php
+++ archive.php
@@ -9,22 +9,34 @@
 							<?php
 							the_archive_title( '<h1 class="archive-page-title">', '</h1>' );
 							the_archive_description( '<div class="taxonomy-description">', '</div>' );
+							
+							
+							//here we make custom wp_query request 
+							
+							
+							
+							
 							?>
+							
+							
 							
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf archive-grid' ); ?> role="article">
 							
-							<div style="color:#ccc;width:30%;float:left;padding:1em;">
+							<div class="article-number">
 							
-							<p class="byline entry-meta vcard">
-										<?php printf( __( 'Posted', 'leonitetheme' ).' %1$s %2$s',
-                  							     /* the time the post was published */
-                  							     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__('by', 'leonitetheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
+								<p class="byline entry-meta vcard number">
+							
+									01
+
+								</p>
+							
+								<p class="byline entry-meta vcard">
+							
+									<?php //_e( "Опубликовано ", 'leonite' ); ?><time class="updated entry-time" datetime="<?php echo get_the_time(' Y/m/d g:i')  ?>" itemprop="datePublished"><?php echo get_the_time('F j, Y g:i') ?></time>
+
+								</p>
 							
 							</div>
 
